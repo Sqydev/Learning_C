532314e149e4484a240cf0979de6e4b508a43c7b
sqyd <wojkap10@proton.me>
2025-03-14 18:50:15 +0100

    cfvgbhjcvbfghjvgb hnjcfvgb jncfvgb hjncvgb njcvgb h

--- notes/Files.c
+++ notes/Files.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    //i DON'T WANNA
+
     return 0;
 }
---
+++ notes/Struckts_and_enums.c
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+static void Struckts() {
+    struct s1 { //Structs are groups of vars
+        int Name;
+        float Height;
+        float Widght;
+    };
+
+    struct s1 mystrc; //to acces struckt you have to make a var of it
+
+    mystrc.Height = 1; //And doing this thingi
+
+    //Btw strings will give error becouse strings in c are fucking retarded:)
+}
+
+
+int main() {
+
+}
